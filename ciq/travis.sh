830a15beefc77abf2551abe8ea8c7556343eb867
Achim <git@achimonline.de>
2016-12-31 00:15:40 +0100

    TravisCI integration

---
+++ ciq/travis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Helper-script for 'Travis CI'-integration
+# (simply download the CIQ-SDK and compile the sources)
+
+SDK_URL="https://developer.garmin.com/downloads/connect-iq/sdks/connectiq-sdk-win-2.2.2.zip"
+
+wget -O sdk.zip "${SDK_URL}"
+unzip sdk.zip -d "sdk"
+
+export MB_HOME="sdk"
+export MB_PRIVATE_KEY="n/a"
+
+./mb_runner.sh build
+
