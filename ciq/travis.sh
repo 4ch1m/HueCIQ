ff23b2a9aa1600f87bd844e95750077c39fce6d0
Achim <git@achimonline.de>
2016-12-31 01:29:43 +0100

    TravisCI integration

--- ciq/travis.sh
+++ ciq/travis.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
 # Helper-script for 'Travis CI'-integration
-# (simply download the CIQ-SDK and compile/package the sources)
-
-SDK_URL="https://developer.garmin.com/downloads/connect-iq/sdks/connectiq-sdk-win-2.2.2.zip"
+#
+# - d/l the CIQ-SDK
+# - generate a dummy private-key for compilation
+# - export the necessary MB-environment-variables
+# - run the actual build-helper-script and package the app
 
 ###
 
-# change to the ciq-subfolder
-cd ciq
+SDK_URL="https://developer.garmin.com/downloads/connect-iq/sdks/connectiq-sdk-win-2.2.2.zip"
 
-# d/l and unzip the SDK
-wget -O sdk.zip "${SDK_URL}"
-unzip sdk.zip -d "sdk"
+SDK_FILE="sdk.zip"
+SDK_DIR="sdk"
 
-# generate a dummy-private-key for compilation
 PEM_FILE="/tmp/developer_key.pem"
 DER_FILE="/tmp/developer_key.der"
+
+###
+
+cd ciq
+
+wget -O "${SDK_FILE}" "${SDK_URL}"
+unzip "${SDK_FILE}" "bin/*" -d "${SDK_DIR}"
+
 openssl genrsa -out "${PEM_FILE}" 4096
 openssl pkcs8 -topk8 -inform PEM -outform DER -in "${PEM_FILE}" -out "${DER_FILE}" -nocrypt
 
-# export the env-vars
-export MB_HOME="sdk"
-export MB_PRIVATE_KEY="/tmp/developer_key.der"
+export MB_HOME="${SDK_DIR}"
+export MB_PRIVATE_KEY="${DER_FILE}"
 
-# run the actual build-helper-script
 ./mb_runner.sh package
